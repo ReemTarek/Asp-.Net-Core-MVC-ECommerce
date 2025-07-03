7ad1d6b0ef7d8f9f662703b68d1fa45d0c87b369
Reem Matr <reem_tarek@outlook.com>
2025-07-03 21:18:32 +0300

    add category

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+           
+        }
+        public DbSet<BulkyWeb.Models.Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BulkyWeb.Models.Category>().HasData(
+                new BulkyWeb.Models.Category
+                {
+                    Id = 1,
+                    Name = "Action",
+                    DisplayOrder = 1
+                },
+                new BulkyWeb.Models.Category
+                {
+                    Id = 2,
+                    Name = "Drama",
+                    DisplayOrder = 2
+                }
+            );
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
